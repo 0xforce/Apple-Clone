fffb6f02ed50844dfb2a3f7b13e669bf5249a0c6
Alberto Abuchaibe <aabuchaibe11@gmail.com>
2024-08-08 16:04:31 -0500

    first commit

---
+++ components/index.jsx
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Highlights from "./Highlights";
+import VideoCarousel from "./VideoCarousel";
+import Model from "./Model";
+import ModelView from "./ModelView";
+import Lights from "./Lights";
+import Features from "./Features";
+import HowItWorks from "./HowItWorks";
+import Footer from "./Footer";
+
+export {
+  Navbar,
+  Hero,
+  Highlights,
+  VideoCarousel,
+  Model,
+  ModelView,
+  Lights,
+  Features,
+  HowItWorks,
+  Footer,
+};
